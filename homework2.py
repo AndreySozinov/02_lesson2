9ed226dd10d871596382fb3ea89fc884c3953ae1
AndreySozinov <savrey@ya.ru>
2023-03-11 16:36:19 +0500

    Second homework done

--- homework2.py
+++ homework2.py
@@ -2,3 +2,35 @@
 # знаменателем. Программа должна возвращать сумму и произведение* дробей.
 # Для проверки своего кода используйте модуль fractions.
 
+import fractions
+import math
+
+fraction1 = input('Введите первую дробь: ').split('/')
+fraction2 = input('Введите вторую дробь: ').split('/')
+
+numer1 = int(fraction1[0])
+denom1 = int(fraction1[1])
+numer2 = int(fraction2[0])
+denom2 = int(fraction2[1])
+
+num_add = numer1 * denom2 + numer2 * denom1
+den_add = denom1 * denom2
+
+divider = math.gcd(num_add, den_add)
+if divider > 0:
+    num_add //= divider
+    den_add //= divider
+
+num_mult = numer1 * numer2
+den_mult = denom1 * denom2
+
+divider = math.gcd(num_mult, den_mult)
+if divider > 0:
+    num_mult //= divider
+    den_mult //= divider
+
+print(f'{numer1}/{denom1} + {numer2}/{denom2} = {num_add}/{den_add}')
+print(f'{numer1}/{denom1} * {numer2}/{denom2} = {num_mult}/{den_mult}')
+
+print(f'Проверка сложения: {fractions.Fraction(numer1, denom1) + fractions.Fraction(numer2, denom2)}')
+print(f'Проверка умножения: {fractions.Fraction(numer1, denom1) * fractions.Fraction(numer2, denom2)}')
